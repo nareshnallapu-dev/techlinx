ab2044f66a0395f9e30ea29c9db5da8e324d1f98
nareshnallapu-dev <naresh.nallapu@techlinx.ai>
2025-12-30 02:14:30 +0530

    added snap

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/techlinx/",
+  build: {
+    target: "es2015" // ✅ FIXES "Unexpected token ?"
+  }
 })
