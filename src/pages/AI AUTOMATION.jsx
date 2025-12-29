c562edd5a4c19428f2b5ecaea0ca2eadbe490e61
nareshnallapu-dev <naresh.nallapu@techlinx.ai>
2025-12-30 01:40:51 +0530

    added snap package

--- src/pages/AI AUTOMATION.jsx
+++ src/pages/AI AUTOMATION.jsx
@@ -8,10 +8,13 @@ import WhyImg from '../assets/whyImg.svg'
 import AutomationImg from '../assets/automationImg.svg'
 import Layout from "../Layout/Layout";
 import TextWidgetPlainAi from '../components/TextWidgetPlainAi';
+import SEO from "./SEO";
+
 const AiAutomation = () => {
     return <>
     <Layout>
-        
+        <SEO title={'Phenom Certified Integration Partner || Techlinx - AI Automation'} description={'Techlinx is an HR technology services company delivering Phenom implementations, AI automation, integrations, and managed services for scalable hiring success. We partner with enterprises to deliver scalable, secure, and high-performing HR technology ecosystems that drive measurable hiring and business outcomes.'} keywords={'Phenom Certified Integration Partner, HR technology services, Phenom implementations, AI in HR, AI-driven HR solutions, Phenom Implementation Partner'} type={"website"}/>
+
         <ImgBannerComponent title={'Intelligent HR Automation That Drives Real Hiring Outcomes'} text={'Techlinx enables enterprises to activate AI-driven automation on Phenom—reducing manual effort, accelerating hiring, and improving candidate and recruiter experiences at scale.'} btnText={parse('Talk to An AI Automation Expert')} ImgBanner={AutomationImg}/>
 
         <ImgTextWidgetTwoPlain title={'Why Techlinx'} ImgBanner={WhyImg} point1={'Deep expertise in Phenom AI & automation'} point2={'Enterprise-ready delivery & QA discipline'} point3={'Proven post-go-live optimization experience'}/>
