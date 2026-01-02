d44fde6bc9f085e26a2ad78b8fbb4e4e3d591dd7
nareshnallapu-dev <naresh.nallapu@techlinx.ai>
2026-01-02 18:24:15 +0530

    made few changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/techlinx/",
-  build: {
-    target: "es2015" // ✅ FIXES "Unexpected token ?"
-  }
 })
